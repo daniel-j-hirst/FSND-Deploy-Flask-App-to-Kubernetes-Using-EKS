6ec6e4c77b28477b3813390283d9ac37f8f89a77
djhir3 <djhir3@gmail.com>
2021-05-27 01:53:25 +1000

    make another update

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import jwt
 from flask import Flask, jsonify, request, abort
 
 # here's something to update the git repo with
+# another update
 
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
